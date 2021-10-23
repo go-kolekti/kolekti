0f0f8138501c0db478631b056acd17f1d28feef8
Henry Huang <hhh@rutcode.com>
2021-10-23 22:13:03 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module trellis.tech/go-kolekti/kolekti
+
+go 1.16
+
+require github.com/iTrellis/common v0.21.18
---
+++ print.go
@@ -0,0 +1,11 @@
+package kolekti
+
+import (
+	"fmt"
+
+	"github.com/iTrellis/common"
+)
+
+func Print() {
+	fmt.Println(common.FormatNamespaceString("kolekti"))
+}
